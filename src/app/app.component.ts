c27e7c6c901b03b34ce29b117f5250fe72172c22
iamhb <its.hari.programmer@gmail.com>
2020-05-07 16:06:57 +0530

    user class added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import { User } from "./models/User";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -11,19 +11,22 @@ export class AppComponent {
   public age: number;
   public newname: string;
   public allData; // holds all db data
+  public user: User;
 
   constructor(public http: HttpClient) {
+    this.user = new User();
     this.showAll();
   }
 
   // submit details to node
   submit() {
-    let userData = {
-      userName: this.name,
-      userAge: this.age,
-    };
+    console.log(this.user);
+    // let userData = {
+    //   userName: this.name,
+    //   userAge: this.age,
+    // };
     this.http
-      .post("http://localhost:3000/submitUser", userData)
+      .post("http://localhost:3000/user/submitUser", this.user)
       .subscribe((apiRes) => {
         // console.log(apiRes);
         this.showAll();
@@ -34,7 +37,7 @@ export class AppComponent {
     this.http
       .get("http://localhost:3000/getAllUserDocs")
       .subscribe((apiRes) => {
-        this.allData = apiRes.docs;
+        this.allData = apiRes["docs"];
       });
   }
 
